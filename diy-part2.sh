94b480b67751b5289a39045902b761fbc270e393
rem1x-hwong <66793624+rem1x-hwong@users.noreply.github.com>
2022-01-30 19:04:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,5 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/R3M1X/g' openwrt/package/base-files/files/bin/config_generate
