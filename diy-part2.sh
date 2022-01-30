c3bd1f9e2330abf3c17b88cc55dd3e377003a0c0
rem1x-hwong <66793624+rem1x-hwong@users.noreply.github.com>
2022-01-31 00:26:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+
+# echo "修改机器名称"
+sed -i "s/OpenWrt/$R3M1X/g" package/base-files/files/bin/config_generate
