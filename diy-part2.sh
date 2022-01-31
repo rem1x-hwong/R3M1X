448fb1a3e2c23c3655fe2fee901210d4925f016a
rem1x-hwong <66793624+rem1x-hwong@users.noreply.github.com>
2022-01-31 21:45:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,9 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 
 # echo "修改机器名称"
 sed -i "s/OpenWrt/R3/g" package/base-files/files/bin/config_generate
+
+# echo '添加Leo-Jo-My的Hello World,并且使用默认包配置'
+git clone $vssr_plus_rely package/lean/luci-vssr-plus-rely
+git clone $vssr_plus_rely package/lean/luci-app-vssr-plus
+echo 'CONFIG_PACKAGE_luci-app-vssr-plus=y' >> .config
+echo 'CONFIG_PACKAGE_luci-i18n-vssr-plus-zh-cn=y'  >> .config
